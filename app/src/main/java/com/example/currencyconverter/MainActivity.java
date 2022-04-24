885398a6690160ec5b63800a38132eed09bdb789
afroCoderHanane <gbadhanane@gmail.com>
2022-04-24 02:04:25 -0700

    currency convert

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convertToUSD(View view) {
+        TextView input = findViewById(R.id.currencyBox);
+        double final_answer = Integer.parseInt(input.getText().toString())*1.08;
+        Toast.makeText(this,"In USD: "+ String.valueOf(final_answer) , Toast.LENGTH_SHORT).show();
     }
 }
